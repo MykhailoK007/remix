dc42ef92d98f08e351f3754397eef636d2bf3ad2
Mykhailo <kotmykhailo2000@gmail.com>
2024-11-12 12:25:27 +0200

    fix i18link ref forwarding

--- app/global/components/i18n-link.tsx
+++ app/global/components/i18n-link.tsx
@@ -1,24 +1,22 @@
 import {Link, LinkProps} from '@remix-run/react';
-import {memo} from 'react';
+import {memo, forwardRef} from 'react';
 import {useTranslation} from 'react-i18next';
 
-//
+export const I18nLink: React.FC<LinkProps> = memo(
+  forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => {
+    const {i18n} = useTranslation();
 
-export const I18nLink: React.FC<LinkProps> = memo((props: LinkProps & LinkProps) => {
-  const {i18n} = useTranslation();
-
-  return <Link {...props} to={parseI18nNavigate(props.to, i18n.language)} />;
-});
+    return <Link ref={ref} {...props} to={parseI18nNavigate(props.to, i18n.language)} />;
+  }),
+);
 
 I18nLink.displayName = 'I18nLink';
 
-//
-
 const parseI18nNavigate = (to: LinkProps['to'], lang: string) => {
-  if (typeof to === 'string' && /^\./.test(to)) return to; // relative path
-  if (typeof to === 'string') return '/' + lang + '/' + String(to).replace(/^\//, ''); // string
+  if (typeof to === 'string' && /^\./.test(to)) return to;
+  if (typeof to === 'string') return '/' + lang + '/' + String(to).replace(/^\//, '');
   if (typeof to === 'object' && to.pathname)
-    return {...to, pathname: '/' + lang + '/' + String(to).replace(/^\//, '')}; // To object
+    return {...to, pathname: '/' + lang + '/' + String(to.pathname).replace(/^\//, '')};
 
-  return to; // fallback
+  return to;
 };
